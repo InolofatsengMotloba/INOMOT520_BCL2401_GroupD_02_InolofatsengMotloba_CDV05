3173abc6fb416bc126ae55f56405db34ad0f38a5
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-02 09:42:46 +0200

    Update image scroll function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   var image = $(".img-profile");
   var aboutPosition = $(".about-myself").offset().top / 0.6;
 
+  // Add a class to hide the image initially
+  image.addClass("hidden");
+
   $(window).on("scroll", function () {
     var scrollTop = $(this).scrollTop();
     image.css({ top: aboutPosition - scrollTop });
@@ -10,5 +13,7 @@ $(document).ready(function () {
       image.css({ top: 0 });
     }
 
+    // Remove the hidden class when the scroll event is triggered
+    image.removeClass("hidden");
   });
 });
